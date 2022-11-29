4626b8c700872817a75cee429ec77f1b69c9108e
achyut kadam <achyut.k@acldigital.com>
2022-11-29 12:34:04 +0530

    AccountType controller specs

--- spec/requests/addresses_spec.rb
+++ spec/requests/addresses_spec.rb
@@ -4,11 +4,9 @@ RSpec.describe 'Addresses', type: :request do
   let!(:addresses) { FactoryBot.create_list(:address, 20) }
   before { get '/addresses/index' }
   it 'returns all addresses' do
-    create_list(:address, 20)
     expect(JSON.parse(response.body).size).to eq(20)
   end
   it 'returns status code 200' do
-    create_list(:address, 20)
     expect(response).to have_http_status(:success)
   end
 end
