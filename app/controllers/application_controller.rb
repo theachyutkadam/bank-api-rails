a657bae4c2fb07bd456c21190ad8e49330904add
achyutkadamacl <achyut.k@acldigital.com>
2023-03-24 21:23:54 +0530

    Added instanec variable on authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::API
   before_action :authenticate_user!
 
   def current_user
-    find_user
+    @login_user
   end
 
   def current_user_information
-    find_user.user_information
+    @login_user.user_information
   end
 
   private
@@ -17,13 +17,13 @@ class ApplicationController < ActionController::API
     if request.authorization
       return render json: { errors: "Invalid token" }, status: :unauthorized unless find_user
 
-      find_user
+      @login_user
     else
       render json: { message: "No authorization Header sent" }, status: :forbidden
     end
   end
 
   def find_user
-    User.find_by(token: request.authorization)
+    @login_user = User.find_by(token: request.authorization)
   end
 end
