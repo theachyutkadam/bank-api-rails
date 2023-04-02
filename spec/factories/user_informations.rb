bcc474e03f50a58d528fdd374e6784a1c56ee209
achyutkadamacl <achyut.k@acldigital.com>
2023-04-03 00:04:16 +0530

    WIP add avatar in factory

--- spec/factories/user_informations.rb
+++ spec/factories/user_informations.rb
@@ -46,5 +46,11 @@ FactoryBot.define do
     adhaar_card_number { Faker::Number.number(digits: 12) }
     is_handicap { false }
     handicap_details { "MyText" }
+
+    # after :create do |user_information|
+    #   file_path = Rails.root.join('spec', 'support', 'assets', 'piano.jpg')
+    #   file = fixture_file_upload(file_path, 'image/jpg')
+    #   user_information.avatar.attach(file)
+    # end
   end
 end
